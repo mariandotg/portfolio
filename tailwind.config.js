096dea4961799a2f435864552091b5179a0ee595
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-17 12:31:07 -0300

    Added Tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,71 @@
+/* eslint-disable prettier/prettier */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    borderRadius: {
+      DEFAULT: '4px',
+    },
+    animation: {
+      'animate-image': 'fade-in 1s ease-out',
+    },
+    keyframes: {
+      'fade-in': {
+        from: {
+          opacity: '0',
+        },
+        to: {
+          opacity: '1',
+        },
+      }
+    },
+    fontSize: {
+      DEFAULT: ['1rem', '1.5rem'],
+      article: ['1rem', '1.175rem'],
+      secondary: ['0.875rem', '0.875rem'],
+      title: ['1.25rem', '1.75rem'],
+    },
+    screens: {
+      mobile: '550px',
+      tablet: '834px',
+      desktop: '1024px',
+    },
+    fontFamily: {
+      display: ['IBM Plex Sans', 'system-ui', 'sans-serif'],
+      body: ['IBM Plex Sans', 'system-ui', 'sans-serif'],
+      monospace: ['IBM Plex Mono', 'system-ui', 'monospace'],
+    },
+    colors: {
+      dark: {
+        DEFAULT: '#0D0D0D',
+        headlines: '#E1E1E1',
+        text: '#B1B1B1',
+        'primary-hover': '#856ADE',
+        'primary-pressed': '#A28DE6',
+        'tertiary-hover': '#555555',
+        'tertiary-pressed': '#7D7D7D',
       },
+      light: {
+        DEFAULT: '#F8F8FA',
+        headlines: '#0D0D0D',
+        text: '#5E5E5E',
+        'primary-hover': '#6246CC',
+        'primary-pressed': '#4A36B9',
+        'tertiary-hover': '#313131',
+        'tertiary-pressed': '#282828',
+      },
+      transparent: 'transparent',
+      primary: '#7050D8',
+      tertiary: '#373737'
     },
   },
   plugins: [],
-}
+};

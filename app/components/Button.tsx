b9ad6b5e6be2b9287ad7bdfcaf940932666eb9b2
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-17 18:42:50 -0300

    Added Button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,61 @@
+import { ReactNode } from 'react';
+
+interface Props {
+  variant: 'primary' | 'secondary' | 'tertiary';
+  children: ReactNode;
+  icon?: boolean;
+  onClick?: () => void;
+  className?: string;
+  url?: string;
+  disabled?: boolean;
+  ariaLabel?: string;
+}
+
+const Button = ({
+  onClick,
+  children,
+  className,
+  variant,
+  url,
+  icon,
+  disabled,
+  ariaLabel,
+}: Props) => {
+  const variants = {
+    primary: 'btn-primary',
+    secondary: 'btn-secondary',
+    tertiary: 'btn-tertiary',
+  };
+  const styles = `${variants[variant]} ${className ? className : ''} ${
+    icon ? 'p-[6px] w-fit rounded-[8px]' : 'px-6 py-3 w-full'
+  } border italic text-center ${
+    disabled && 'disabled:opacity-30 disabled:pointer-events-none'
+  } flex items-center justify-center gap-2 text-secondary whitespace-nowrap transition rounded font-monospace`;
+
+  if (url) {
+    return (
+      <a
+        className={styles}
+        onClick={onClick}
+        href={url}
+        target='_blank'
+        rel='noreferrer'
+      >
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button
+        className={styles}
+        onClick={onClick}
+        disabled={disabled}
+        aria-label={icon ? ariaLabel : undefined}
+      >
+        {children}
+      </button>
+    );
+  }
+};
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { pageContentAdapter } from '@/adapters/pageContentAdapter';
 import { IPage } from '@/models/contentful/generated/contentful';
 import { getContentfulData } from '@/services/contentful';
 import Section from './components/Section';
+import Button from './components/Button';
 
 async function fetchTest() {
   const response = await getContentfulData<IPage>({
@@ -16,7 +17,11 @@ const HomePage = async () => {
   console.log(pageContentAdapter(data[0].fields.sections));
   return (
     <Section>
-      <div className='col-span-3 bg-primary'>test</div>
+      <div className='col-span-3'>
+        <Button variant='primary'>Test button</Button>
+        <Button variant='secondary'>Test button</Button>
+        <Button variant='tertiary'>Test button</Button>
+      </div>
     </Section>
   );
 };

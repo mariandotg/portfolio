8b67a4e9c6978cbc4e452bcfbdb067a15323e3d8
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-18 17:48:20 -0300

    Added simple ProjectPage

---
+++ app/projects/[path]/page.tsx
@@ -0,0 +1,59 @@
+import PageLayout from '@/app/components/PageLayout';
+import { getNotionSinglePage } from '@/services/notion';
+import React from 'react';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+
+interface Props {
+  params: {
+    path: string;
+  };
+}
+
+const ProjectPage = async ({ params }: Props) => {
+  const databaseId = process.env.NEXT_PUBLIC_NOTION_PAGES_DATABASE_ID!;
+
+  const blogResponse = await getNotionSinglePage({
+    databaseId,
+    filter: {
+      property: 'SeoPath',
+      formula: {
+        string: {
+          equals: params.path,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <div className='relative h-64'></div>
+      <PageLayout>
+        <div className='flex flex-col gap-y-4'>
+          <ReactMarkdown
+            components={{
+              h1: ({ node, ...props }) => (
+                <h1
+                  className='font-medium text-title dark:text-dark-headlines text-light-headlines'
+                  {...props}
+                />
+              ),
+              h2: ({ node, ...props }) => (
+                <h2
+                  className='italic font-medium font-monospace dark:text-dark-headlines text-light-headlines'
+                  {...props}
+                />
+              ),
+              p: ({ node, ...props }) => (
+                <p className='dark:text-dark-text text-light-text' {...props} />
+              ),
+            }}
+          >
+            {blogResponse?.markdown.parent}
+          </ReactMarkdown>
+        </div>
+      </PageLayout>
+    </>
+  );
+};
+
+export default ProjectPage;

ed54662648f957ff1a828cbd5ae735c4ef23a8aa
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-18 18:01:58 -0300

    Added basic pageSeoAdapter

---
+++ adapters/pageSeoAdapter.ts
@@ -0,0 +1,17 @@
+import { PageSeo } from "@/models/PageSeo";
+
+export const pageSeoAdapter = (post: any): Omit<PageSeo, 'loading'> => {
+  console.log(post);
+  return {
+    id: post.id,
+    title: post.properties.SeoTitle.rich_text[0].plain_text,
+    description: post.properties.Description.rich_text[0].plain_text,
+    image: post.properties.Image.files[0].external.url,
+    imageAlt: post.properties.ImageAlt.rich_text[0].plain_text,
+    slug: post.properties.SeoSlug.rich_text[0].plain_text,
+    path: post.properties.SeoPath.formula.string,
+    url: post.properties.SeoUrl.rich_text[0].plain_text,
+    openGraphType: post.properties.SeoOpenGraphType.rich_text[0].plain_text,
+    schemaType: post.properties.SeoSchemaType.rich_text[0].plain_text,
+  };
+};

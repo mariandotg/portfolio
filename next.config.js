a70c28f121eae64cfe26b3b68a091b096a205112
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-18 08:32:39 -0300

    Added image domains to next.onfig file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: '',
+        },
+      ],
+    },
+}
 
 module.exports = nextConfig

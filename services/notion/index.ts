6e324031e7a7bcbad4083107d81208e004e167a3
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-18 17:47:58 -0300

    Added getNotionSinglePage to notion service

--- services/notion/index.ts
+++ services/notion/index.ts
@@ -5,9 +5,13 @@ import { NotionResponse } from '@/models/notion/NotionResponse';
 
 const { Client } = require('@notionhq/client');
 
+const { NotionToMarkdown } = require('notion-to-md');
+
 const client = new Client({
   auth: process.env.NEXT_PUBLIC_NOTION_AUTH_TOKEN,
 });
+
+const n2m = new NotionToMarkdown({ notionClient: client });
 
 interface Props {
   databaseId: string;
@@ -24,4 +28,23 @@ export const queryNotionDatabase = async ({
   });
 
   return notionDataAdapter(response);
+};
+
+export const getNotionSinglePage = async ({
+  databaseId,
+  filter,
+}: Props): Promise<any> => {
+  const response: NotionClientQueryResponse = await client.databases.query({
+    database_id: databaseId,
+    filter,
+  });
+
+  const page = response.results[0];
+  const mdblocks = await n2m.pageToMarkdown(page.id);
+  const mdString = n2m.toMarkdownString(mdblocks);
+
+  console.log('mdString', mdString);
+  return {
+    markdown: mdString,
+  };
 };

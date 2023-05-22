0f39b4d85bd55680a26915d55627a621173a5608
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-22 13:33:54 -0300

    Added caption to project images

--- app/[lang]/projects/[path]/page.tsx
+++ app/[lang]/projects/[path]/page.tsx
@@ -87,14 +87,17 @@ const ProjectPage = async ({ params }: Props) => {
                 />
               ),
               img: ({ node, src, alt, width, height }) => (
-                <div className='relative h-64'>
-                  <Image
-                    src={src!}
-                    alt={alt!}
-                    fill={true}
-                    className='absolute object-cover rounded'
-                  />
-                </div>
+                <>
+                  <div className='relative h-64 mb-2'>
+                    <Image
+                      src={src!}
+                      alt={alt!}
+                      fill={true}
+                      className='absolute object-cover rounded'
+                    />
+                  </div>
+                  <p className='text-secondary'>{alt}</p>
+                </>
               ),
             }}
           >

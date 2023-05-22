08541b375cb7a7eb179148162702b74a4505d75a
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-22 17:10:18 -0300

    Better article page

--- app/[lang]/blog/[path]/page.tsx
+++ app/[lang]/blog/[path]/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PageLayout from '@/components/PageLayout';
-import { getNotionSinglePage } from '@/services/notion';
+import { getNotionSinglePage, queryNotionDatabase } from '@/services/notion';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { articlesAdapter } from '@/adapters/articlesAdapter';
+import Image from 'next/image';
+import SkillItem from '@/components/SkillItem';
 
 interface Props {
   params: {
@@ -24,10 +27,49 @@ const ArticlePage = async ({ params }: Props) => {
     },
   });
 
+  const blogMetadata = articlesAdapter(
+    await queryNotionDatabase({
+      databaseId,
+      filter: {
+        property: 'SeoPath',
+        formula: {
+          string: {
+            equals: params.path,
+          },
+        },
+      },
+    })
+  );
+
   return (
-    <>
-      <div className='relative h-64'></div>
-      <PageLayout>
+    <PageLayout>
+      <div className='flex flex-col gap-y-8 dark:text-dark-text text-light-text'>
+        <div className='flex flex-col gap-y-2'>
+          <div className='relative h-64 mt-20'>
+            <Image
+              src={blogMetadata[0].image}
+              alt='page header'
+              className='absolute object-cover rounded'
+              fill={true}
+              priority
+              quality={90}
+            />
+          </div>
+          <div className='flex flex-col gap-y-4'>
+            <div className='flex flex-col gap-y-2'>
+              <h1 className='font-medium text-title dark:text-dark-headlines text-light-headlines'>
+                {blogMetadata[0].name}
+              </h1>
+            </div>
+            <ul className='flex flex-row flex-wrap items-center w-full gap-2'>
+              {blogMetadata[0].tags.map((tag, index) => (
+                <li>
+                  <SkillItem key={tag.id} skill={tag.name} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
         <div className='flex flex-col gap-y-4'>
           <ReactMarkdown
             components={{
@@ -43,16 +85,33 @@ const ArticlePage = async ({ params }: Props) => {
                   {...props}
                 />
               ),
-              p: ({ node, ...props }) => (
-                <p className='dark:text-dark-text text-light-text' {...props} />
+              img: ({ node, src, alt, width, height }) => (
+                <>
+                  <div className='relative h-64 mb-2'>
+                    <Image
+                      src={src!}
+                      alt={alt!}
+                      fill={true}
+                      className='absolute object-cover rounded'
+                    />
+                  </div>
+                  <p className='text-secondary'>{alt}</p>
+                </>
               ),
+              ul: ({ node, ...props }) => (
+                <ul className='ml-4 list-disc' {...props} />
+              ),
+              pre: ({ node, children, ...props }) => {
+                console.log(children);
+                return <pre className='text-primary' {...props} />;
+              },
             }}
           >
             {blogResponse?.markdown.parent}
           </ReactMarkdown>
         </div>
-      </PageLayout>
-    </>
+      </div>
+    </PageLayout>
   );
 };
 

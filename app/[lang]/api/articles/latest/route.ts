0fe2ed327301b391b132a63e4ecd4d4034e9c23f
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-25 16:12:20 -0300

    Added latest articles api route

---
+++ app/[lang]/api/articles/latest/route.ts
@@ -0,0 +1,41 @@
+import { articlesAdapter } from '@/adapters/articlesAdapter';
+import { CompoundFilterObj } from '@/models/notion/Filters';
+import { queryNotionDatabase } from '@/services/notion';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const lang = request.nextUrl.pathname.split('/').filter(Boolean)[0];
+  const databaseId = process.env.NEXT_PUBLIC_NOTION_PAGES_DATABASE_ID!;
+
+  const articlesFilter: CompoundFilterObj = {
+    and: [
+      {
+        property: 'Stage',
+        select: {
+          equals: 'Published',
+        },
+      },
+      {
+        property: 'LatestArticle',
+        checkbox: {
+          equals: true,
+        },
+      },
+      {
+        property: 'Database',
+        select: {
+          equals: 'Articles Database',
+        },
+      },
+    ],
+  };
+
+  const articlesResponse = await queryNotionDatabase({
+    databaseId,
+    filter: articlesFilter,
+  });
+
+  const articles = articlesAdapter(articlesResponse);
+
+  return NextResponse.json({ articles });
+}

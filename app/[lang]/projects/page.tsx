5f4b8bb32196614080f0d6e3e25654c8c08a473a
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-29 17:52:59 -0300

    Added Projects page

--- app/[lang]/projects/page.tsx
+++ app/[lang]/projects/page.tsx
@@ -1,22 +1,36 @@
 import Image from 'next/image';
 import React from 'react';
 import PageLayout from '../../../components/PageLayout';
+import ProjectCard from '@/components/ProjectCard';
+import { Project } from '@/models/domain/Project';
+import SectionTitle from '@/components/SectionTitle';
+import Section from '@/components/Section';
 
-const ProjectsPage = () => {
+const ProjectsPage = async () => {
+  const projectsFetch = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/en/api/projects`,
+    { cache: 'no-cache' }
+  );
+
+  const projectsResponse: { projects: Project[] } = await projectsFetch.json();
+
   return (
     <PageLayout>
-      <div className='flex flex-col gap-8'>
-        <div className='relative h-64'>
-          <Image
-            src='/header-web.webp'
-            alt='page header'
-            className='absolute object-cover'
-            fill={true}
-            priority
-            quality={90}
-          />
+      <Section>
+        <SectionTitle emoji='pin'>Projects</SectionTitle>
+        <div className='flex flex-col gap-y-8 mobile:grid mobile:grid-cols-2 mobile:gap-4 tablet:col-span-3 tablet:grid-cols-3 tablet:grid-rows-3 tablet:gap-4'>
+          {projectsResponse.projects.map((project, index) => (
+            <ProjectCard
+              key={project.id}
+              project={project}
+              className={
+                index === 0 ? 'mobile:col-span-2' : 'mobile:col-span-1'
+              }
+              locale={'/en'}
+            />
+          ))}
         </div>
-      </div>
+      </Section>
     </PageLayout>
   );
 };

4d7e5166f4e0bd7e8e40a736dd9aa9ab1b4bbf90
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-06-03 20:39:15 -0300

    Testing loading page

--- app/[lang]/blog/[path]/layout.tsx
+++ app/[lang]/blog/[path]/layout.tsx
@@ -3,6 +3,11 @@ import { Metadata } from 'next';
 import { PageSeo } from '@/models/PageSeo';
 import { metadataAdapter } from '@/adapters/metadataAdapter';
 import { Article } from '@/models/domain/Article';
+import ArticleCard from '@/components/ArticleCard';
+import PageLayout from '@/components/PageLayout';
+import Section from '@/components/Section';
+import SectionTitle from '@/components/SectionTitle';
+import Image from 'next/image';
 
 interface Props {
   params: {
@@ -12,9 +17,9 @@ interface Props {
 }
 
 interface ArticleData {
-  markdown: { parent: string };
+  content: { parent: string };
   seo: Omit<PageSeo, 'loading'>;
-  metadata: Article;
+  properties: Article;
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
@@ -28,15 +33,70 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return metadataAdapter(articleResponse.seo);
 }
 
-const ArticleLayout = ({
+const ArticleLayout = async ({
   children,
+  params,
 }: {
   children: React.ReactNode;
   params: {
     lang: string;
+    path: string;
   };
 }) => {
-  return children;
+  const articleFetch = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/${params.lang}/api/articles/${params.path}`,
+    { cache: 'no-cache' }
+  );
+
+  const articleResponse: ArticleData = await articleFetch.json();
+
+  const latestArticlesFetch = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/${params.lang}/api/articles/latest`,
+    { cache: 'no-cache' }
+  );
+
+  const latestArticlesResponse: Article[] = await latestArticlesFetch.json();
+
+  return (
+    <PageLayout>
+      <div className='flex flex-col gap-y-8 dark:text-dark-text text-light-text tablet:grid tablet:grid-cols-3 tablet:gap-4'>
+        <div className='flex flex-col col-span-3 gap-y-2'>
+          <div className='relative h-64 tablet:col-span-3'>
+            <Image
+              src={articleResponse.properties.image}
+              alt='page header'
+              className='absolute object-cover rounded'
+              fill={true}
+              priority
+              quality={90}
+              placeholder='blur'
+              blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAFCAIAAADzBuo/AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAASSURBVBhXY3gro4IHDZy0jAoA9QM6yzHo/PoAAAAASUVORK5CYII='
+            />
+          </div>
+        </div>
+        {children}
+      </div>
+      <Section>
+        <SectionTitle emoji='article'>Latest Articles</SectionTitle>
+        <div className='flex w-full snap-x tablet:col-span-3'>
+          <ul className='flex flex-col w-full gap-4 mobile:grid mobile:grid-cols-2 tablet:grid-cols-3'>
+            {latestArticlesResponse.map((article, index) => (
+              <li
+                key={article.id}
+                className={`cursor-pointer group ${
+                  index === 0
+                    ? 'mobile:col-span-2 tablet:col-span-1'
+                    : 'mobile:col-span-1'
+                }`}
+              >
+                <ArticleCard article={article} locale={params.lang} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Section>
+    </PageLayout>
+  );
 };
 
 export default ArticleLayout;
--- app/[lang]/blog/[path]/page.tsx
+++ app/[lang]/blog/[path]/page.tsx
@@ -29,90 +29,50 @@ const ArticlePage = async ({ params }: Props) => {
     `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/${params.lang}/api/articles/${params.path}`,
     { cache: 'no-cache' }
   );
-  const latestArticlesFetch = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/${params.lang}/api/articles/latest`,
-    { cache: 'no-cache' }
-  );
 
   const articleResponse: ArticleData = await articleFetch.json();
-  const latestArticlesResponse: Article[] = await latestArticlesFetch.json();
 
   const { formattedDate } = useDate(
     new Date(articleResponse.properties.date.start!)
   );
 
   return (
-    <PageLayout>
-      <div className='flex flex-col gap-y-8 dark:text-dark-text text-light-text tablet:grid tablet:grid-cols-3 tablet:gap-4'>
-        <div className='flex flex-col col-span-3 gap-y-2'>
-          <div className='relative h-64 tablet:col-span-3'>
-            <Image
-              src={articleResponse.properties.image}
-              alt='page header'
-              className='absolute object-cover rounded'
-              fill={true}
-              priority
-              quality={90}
-              placeholder='blur'
-              blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAFCAIAAADzBuo/AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAASSURBVBhXY3gro4IHDZy0jAoA9QM6yzHo/PoAAAAASUVORK5CYII='
-            />
-          </div>
-        </div>
-        <div className='flex flex-col gap-y-4 tablet:col-span-2'>
-          <div className='flex flex-col gap-y-4'>
-            <div className='flex flex-col gap-y-2'>
-              <h1 className='font-medium text-title dark:text-dark-headlines text-light-headlines'>
-                {articleResponse.properties.name}
-              </h1>
-              {articleResponse.properties.categories.map((tag, index) => (
-                <span className='flex px-2 italic font-medium rounded font-monospace text-dark-headlines bg-dark-tertiary-hover dark:bg-light-tertiary-hover w-fit'>
-                  {(tag.name as String).toLocaleUpperCase()}
-                </span>
-              ))}
-            </div>
-            <p className='dark:text-dark-text text-light-text'>
-              {articleResponse.properties.description}
-            </p>
-          </div>
-          <Markdown>{articleResponse.content.parent}</Markdown>
-        </div>
-        <div className='relative flex flex-col col-span-1 gap-y-4'>
+    <>
+      <div className='flex flex-col gap-y-4 tablet:col-span-2'>
+        <div className='flex flex-col gap-y-4'>
           <div className='flex flex-col gap-y-2'>
-            <h3 className='italic font-medium font-monospace dark:text-dark-headlines text-light-headlines'>
-              Tags:
-            </h3>
-            <ul className='flex flex-row flex-wrap items-center w-full gap-2'>
-              {articleResponse.properties.tags.map((tag, index) => (
-                <li>
-                  <SkillItem key={tag.id} skill={tag.name} />
-                </li>
-              ))}
-            </ul>
+            <h1 className='font-medium text-title dark:text-dark-headlines text-light-headlines'>
+              {articleResponse.properties.name}
+            </h1>
+            {articleResponse.properties.categories.map((tag, index) => (
+              <span className='flex px-2 italic font-medium rounded font-monospace text-dark-headlines bg-dark-tertiary-hover dark:bg-light-tertiary-hover w-fit'>
+                {(tag.name as String).toLocaleUpperCase()}
+              </span>
+            ))}
           </div>
-          <PageIndex />
+          <p className='dark:text-dark-text text-light-text'>
+            {articleResponse.properties.description}
+          </p>
         </div>
-        <p>Published: {formattedDate[params.lang]}</p>
+        <Markdown>{articleResponse.content.parent}</Markdown>
       </div>
-      <Section>
-        <SectionTitle emoji='article'>Latest Articles</SectionTitle>
-        <div className='flex w-full snap-x tablet:col-span-3'>
-          <ul className='flex flex-col w-full gap-4 mobile:grid mobile:grid-cols-2 tablet:grid-cols-3'>
-            {latestArticlesResponse.map((article, index) => (
-              <li
-                key={article.id}
-                className={`cursor-pointer group ${
-                  index === 0
-                    ? 'mobile:col-span-2 tablet:col-span-1'
-                    : 'mobile:col-span-1'
-                }`}
-              >
-                <ArticleCard article={article} locale={params.lang} />
+      <div className='relative flex flex-col col-span-1 gap-y-4'>
+        <div className='flex flex-col gap-y-2'>
+          <h3 className='italic font-medium font-monospace dark:text-dark-headlines text-light-headlines'>
+            Tags:
+          </h3>
+          <ul className='flex flex-row flex-wrap items-center w-full gap-2'>
+            {articleResponse.properties.tags.map((tag, index) => (
+              <li>
+                <SkillItem key={tag.id} skill={tag.name} />
               </li>
             ))}
           </ul>
         </div>
-      </Section>
-    </PageLayout>
+        <PageIndex />
+      </div>
+      <p>Published: {formattedDate[params.lang]}</p>
+    </>
   );
 };
 

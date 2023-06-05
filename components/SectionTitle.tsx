f8d223d3616e4d14f8c7b9a64ed746c41e05f6a1
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-06-05 12:01:22 -0300

    Removed after styles from SectionTitle

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from 'react';
-
-import { Button as ButtonModel } from '@/models/domain/Button';
 import Emoji from './Emoji';
 
 interface Props {
@@ -12,7 +10,7 @@ const SectionTitle = ({ children, emoji }: Props) => {
   return (
     <div className='flex justify-between tablet:col-span-3'>
       <h2
-        className={`flex italic font-medium items-center after:content-[""] after:h-[1px] after:grow w-full after:bg-primary dark:text-dark-headlines gap-x-4 font-monospace text-light-headlines`}
+        className={`flex italic font-medium items-center w-full dark:text-dark-headlines gap-x-4 font-monospace text-light-headlines`}
       >
         <Emoji emoji={emoji} width={20} height={20} />
         {children}

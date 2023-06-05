4fb79477611de49a8a6d07274fdb9b43234b9576
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-06-05 08:03:26 -0300

    Added skeleton loading on project and article page

--- app/[lang]/blog/[path]/loading.tsx
+++ app/[lang]/blog/[path]/loading.tsx
@@ -1,11 +1,64 @@
 const Loading = () => {
   return (
     <>
-      <div className='flex flex-col gap-y-4 tablet:col-span-2 bg-primary'>
-        loading article
+      <div className='flex flex-col gap-y-4 tablet:col-span-2'>
+        <div className='flex flex-col gap-y-2'>
+          <div className='w-full rounded h-7 bg-tertiary animate-pulse'></div>
+          <div className='w-24 h-6 rounded bg-tertiary animate-pulse'></div>
+        </div>
+        <div className='flex flex-col gap-y-2'>
+          {[...Array(5).keys()].map((item) => (
+            <div
+              key={item}
+              className={`${
+                item === 4 ? 'w-1/3' : 'w-full'
+              } h-4 rounded bg-tertiary animate-pulse`}
+            ></div>
+          ))}
+        </div>
+        {[...Array(4).keys()].map((item) => (
+          <div key={item} className='flex flex-col gap-y-4'>
+            <div className='w-1/2 h-6 rounded bg-tertiary animate-pulse'></div>
+            <div className='flex flex-col gap-y-2'>
+              {[...Array(5).keys()].map((item) => (
+                <div
+                  key={item}
+                  className={`${
+                    item === 4 ? 'w-1/3' : 'w-full'
+                  } h-4 rounded bg-tertiary animate-pulse`}
+                ></div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
-      <div className='relative flex flex-col col-span-1 gap-y-4 bg-primary'>
-        loading sidebar
+      <div className='sidebar'>
+        <div className='sidebar-group'>
+          <span className='w-full h-6 rounded bg-tertiary animate-pulse'></span>
+          <div className='flex flex-wrap gap-2'>
+            {[...Array(3).keys()].map((item) => (
+              <span
+                key={item}
+                className='h-6 rounded w-28 bg-tertiary animate-pulse'
+              ></span>
+            ))}
+          </div>
+        </div>
+        <div className='sidebar-group'>
+          <div className='w-full h-6 rounded bg-tertiary animate-pulse'></div>
+          <ul className='flex flex-col w-full gap-y-2'>
+            {[...Array(5).keys()].map((item) => (
+              <li
+                key={item}
+                className='w-full h-[14px] rounded bg-tertiary animate-pulse'
+              ></li>
+            ))}
+          </ul>
+        </div>
+        <div className='sidebar-group'>
+          <div className='w-full h-6 rounded bg-tertiary animate-pulse'></div>
+          <span className='w-full h-6 rounded bg-tertiary animate-pulse'></span>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -66,6 +66,19 @@ module.exports = {
       primary: '#7050D8',
       tertiary: '#373737'
     },
+    animation: {
+      'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+    },
+    keyframes: {
+      pulse: {
+        "0%, 100%": {
+          opacity: 1,
+        },
+        "50%": {
+          opacity: 0.4,
+        }
+      }
+    }
   },
   plugins: [],
 };

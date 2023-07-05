36836d9544a13f8cdebce02a4c8439c6365fce50
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-07-05 19:48:04 -0300

    Added createFiltersFromParam method on api

--- app/[lang]/api/projects/route.ts
+++ app/[lang]/api/projects/route.ts
@@ -1,11 +1,41 @@
 import { projectsAdapter } from '@/adapters/projectsAdapter';
-import { CompoundFilterObj } from '@/models/notion/Filters';
+import { CompoundFilterObj, FilterObj } from '@/models/notion/Filters';
 import { queryDatabase } from '@/services/notion';
 import { NextRequest, NextResponse } from 'next/server';
+
+const createFiltersFromParam = (
+  searchParams: URLSearchParams,
+  param: string
+) => {
+  const tags = searchParams.get(param);
+
+  const values = tags?.split(',');
+  const filters = values?.reduce<Array<FilterObj<{ [key: string]: unknown }>>>(
+    (acc, currValue) => {
+      return [
+        ...acc,
+        {
+          property: 'Tags',
+          multi_select: {
+            contains: currValue,
+          },
+        },
+      ];
+    },
+    []
+  );
+
+  return filters || [];
+};
 
 export async function GET(request: NextRequest) {
   const lang = request.nextUrl.pathname.split('/').filter(Boolean)[0];
   const databaseId = process.env.NEXT_PUBLIC_NOTION_PAGES_DATABASE_ID!;
+
+  const tagsFilters = createFiltersFromParam(
+    request.nextUrl.searchParams,
+    'tags'
+  );
 
   const projectsFilter: CompoundFilterObj = {
     and: [
@@ -27,6 +57,7 @@ export async function GET(request: NextRequest) {
           equals: lang,
         },
       },
+      ...tagsFilters,
     ],
   };
 

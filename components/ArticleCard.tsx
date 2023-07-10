26aeaf9f60fec2e776c826c6830ceba5e9f6e14b
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-07-10 11:54:16 -0300

    Modified ArticleCard styles

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -14,30 +14,30 @@ interface Props {
 const ArticleCard = ({ article, locale, path }: Props) => {
   const { formattedDate } = useDate(new Date(article.date.start!));
   return (
-    <Link href={path} className='flex flex-col gap-y-2'>
+    <div className='flex flex-col gap-y-2'>
       <span className='font-light text-secondary text-light-text dark:text-dark-text'>
-        {formattedDate[locale]}
+        <span className='mr-2'>{formattedDate[locale]}</span>
+        <Link href={'/test'} className='underline hover:text-primary'>
+          {article.categories[0].name}
+        </Link>
       </span>
-      <div className='relative flex w-full overflow-hidden h-[135px] tablet:h-[135px] rounded'>
-        <h3 className='absolute z-20 font-medium bottom-2 left-2 text-article text-dark-headlines font-display'>
+      <Link href={path} className='flex flex-col gap-y-2'>
+        <div className='relative flex w-full overflow-hidden h-[135px] tablet:h-[135px] rounded'>
+          <div className='absolute top-0 left-0 z-10 flex items-end justify-center w-full h-full p-4 opacity-100 tablet:opacity-0 group-hover:opacity-100 tablet:bg-dark/25'>
+            <MdArrowForward className='absolute w-5 h-5 opacity-100 tablet:opacity-0 right-2 top-2 text-dark-headlines group-hover:opacity-100' />
+          </div>
+          <Image
+            src={article.image}
+            alt={`${article.name} image`}
+            className='top-0 left-0 object-cover rounded aspect-video'
+            fill={true}
+          />
+        </div>
+        <h3 className='font-medium text-title text-dark-headlines font-display'>
           {article.name}
         </h3>
-        <div className='absolute top-0 left-0 z-10 flex items-end justify-center w-full h-full p-4 opacity-100 tablet:opacity-0 group-hover:opacity-100 tablet:bg-dark/25'>
-          <MdArrowForward className='absolute w-5 h-5 opacity-100 tablet:opacity-0 right-2 top-2 text-dark-headlines group-hover:opacity-100' />
-        </div>
-        <Image
-          src={article.image}
-          alt={`${article.name} image`}
-          className='top-0 left-0 object-cover rounded aspect-video'
-          fill={true}
-        />
-      </div>
-      <div className='flex flex-row items-center w-full gap-2'>
-        <span className='flex px-2 py-1 italic rounded text-secondary font-monospace text-light bg-tertiary w-fit'>
-          {article.categories[0].name}
-        </span>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 

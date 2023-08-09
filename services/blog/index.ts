dd54a10fc720a9ece7b6b2cbee55ebdcfbf37089
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-09 05:53:25 -0300

    Added config file

--- services/blog/index.ts
+++ services/blog/index.ts
@@ -1,14 +1,20 @@
 /*
 TODO: Método para obtener latest articles (minified: Title, Image, Category, Path, Id)
+http://localhost:1337/api/articles?locale=en&populate[image]=*&fields[0]=title&fields[1]=publishedAt&fields[2]=category&fields[3]=path&pagination[pageSize]=3&sort[0]=publishedAt:desc
+
 TODO: Método para obtener los articles (paginados) (minified: Title, Image, Category, Path, Id)
+http://localhost:1337/api/articles?locale=en&populate[image]=*&fields[0]=title&fields[1]=publishedAt&fields[2]=category&fields[3]=path
+
 TODO: Método para obtener el article individual (todo)
+http://localhost:1337/api/articles/1?populate[image]=*&populate[localizations][fields][1]=title
 */
 
-const url = 'http://127.0.0.1:1337/';
+import { NEXT_PUBLIC_API_URL } from '@/config';
 
 export const getArticles = async () => {
   const response = await fetch(
-    `${url}api/articles?locale=en&populate[image]=*&fields[0]=title&fields[1]=publishedAt&fields[2]=category&fields[3]=path`
+    `${NEXT_PUBLIC_API_URL}/articles?locale=en&populate[image]=*&fields[0]=title&fields[1]=publishedAt&fields[2]=category&fields[3]=path`,
+    { cache: 'no-cache' }
   );
   if (!response.ok) {
     throw new Error('test error');

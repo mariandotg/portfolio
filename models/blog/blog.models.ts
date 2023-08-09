b2122fbf8c90fd909f1db3a5ca2e161091a55505
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-09 07:28:39 -0300

    Added base blog service models

---
+++ models/blog/blog.models.ts
@@ -0,0 +1,98 @@
+export interface PreviewArticle {
+  id: number;
+  title: string;
+  publishedAt: string;
+  category: string;
+  path: string;
+  image: {
+    placeholder: string;
+    name: string;
+    alternativeText?: string;
+    width: number;
+    height: number;
+    url: string;
+  };
+}
+
+export interface RawArticle {
+  id: number;
+  attributes: Attribute;
+}
+
+export interface RawPreviewArticle {
+  id: number;
+  attributes: Pick<
+    Attribute,
+    'title' | 'publishedAt' | 'category' | 'path' | 'image'
+  >;
+}
+
+export interface Attribute {
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  locale: string;
+  description: string;
+  path: string;
+  category: string;
+  canonicalUrl: string;
+  openGraphType: string;
+  schemaType: string;
+  image: RawImage;
+  localizations: {
+    data: [
+      {
+        id: number;
+        attributes: {
+          title: string;
+        };
+      }
+    ];
+  };
+}
+
+export interface RawImage {
+  data: {
+    id: number;
+    attributes: {
+      name: string;
+      alternativeText?: string;
+      caption?: string;
+      width: number;
+      height: number;
+      formats: {
+        [key: string]: {
+          ext: string;
+          url: string;
+          hash: string;
+          mime: string;
+          name: string;
+          path?: string;
+          size: number;
+          width: number;
+          height: number;
+          provider_metadata: {
+            public_id: string;
+            resource_type: string;
+          };
+        };
+      };
+      hash: string;
+      ext: string;
+      mime: string;
+      size: number;
+      url: string;
+      previewUrl: string;
+      provider: string;
+      provider_metadata: {
+        public_id: string;
+        resource_type: string;
+      };
+      createdAt: string;
+      updatedAt: string;
+      placeholder: string;
+    };
+  };
+}

73a070a2a44e1efeea4191568acac2ee53a41900
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-12 01:14:45 -0300

    Refactored articles api route

--- app/[lang]/api/articles/route.ts
+++ app/[lang]/api/articles/route.ts
@@ -1,55 +1,13 @@
-import { articlesAdapter } from '@/adapters/articlesAdapter';
-import { CompoundFilterObj } from '@/models/notion/Filters';
-import { queryDatabase } from '@/services/notion';
+import { rawPreviewArticlesAdapter } from '@/adapters/rawPreviewArticlesAdapter';
+import { getArticles } from '@/services/blog';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const lang = request.nextUrl.pathname.split('/').filter(Boolean)[0];
   const category = request.nextUrl.searchParams.get('category');
-  const databaseId = process.env.NEXT_PUBLIC_NOTION_PAGES_DATABASE_ID!;
 
-  const categoryFilter = category
-    ? [
-        {
-          property: 'Category',
-          select: {
-            equals: category,
-          },
-        },
-      ]
-    : [];
-
-  const articlesFilter: CompoundFilterObj = {
-    and: [
-      {
-        property: 'Stage',
-        select: {
-          equals: 'Published',
-        },
-      },
-      {
-        property: 'Database',
-        select: {
-          equals: 'Articles Database',
-        },
-      },
-      {
-        property: 'Locale',
-        select: {
-          equals: lang,
-        },
-      },
-      ...categoryFilter,
-    ],
-  };
-
-  const articlesResponse = await queryDatabase({
-    databaseId,
-    filter: articlesFilter,
-    pageSize: 10,
-  });
-
-  const articles = articlesAdapter(articlesResponse);
+  const data = await getArticles(lang);
+  const articles = rawPreviewArticlesAdapter(data);
 
   return NextResponse.json({ articles });
 }

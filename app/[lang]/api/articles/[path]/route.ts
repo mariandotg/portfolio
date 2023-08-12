0d38cfd6e8bb59c9a830cb09a9f4c6b0bf9ccbcd
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-12 01:15:44 -0300

    Refactored single article api route

--- app/[lang]/api/articles/[path]/route.ts
+++ app/[lang]/api/articles/[path]/route.ts
@@ -1,48 +1,15 @@
-import { articlesAdapter } from '@/adapters/articlesAdapter';
-import { CompoundFilterObj } from '@/models/notion/Filters';
-import { getPageData } from '@/services/notion';
+import { rawFullArticleAdapter } from '@/adapters/rawFullArticleAdapter';
+import { getArticle } from '@/services/blog';
 import { notFound } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const lang = request.nextUrl.pathname.split('/').filter(Boolean)[0];
   const path = request.nextUrl.pathname.split('/').filter(Boolean)[3];
-  const databaseId = process.env.NEXT_PUBLIC_NOTION_PAGES_DATABASE_ID!;
 
-  console.log('lang blog article', lang, 'path', path);
-  const articleFilter: CompoundFilterObj = {
-    and: [
-      {
-        property: 'Stage',
-        select: {
-          equals: 'Published',
-        },
-      },
-      {
-        property: 'SeoPath',
-        formula: {
-          string: {
-            equals: path,
-          },
-        },
-      },
-      {
-        property: 'Locale',
-        select: {
-          equals: lang,
-        },
-      },
-    ],
-  };
-
-  const articleResponse = await getPageData(
-    {
-      databaseId,
-      filter: articleFilter,
-      pageSize: 1,
-    },
-    { seo: true, properties: { adapter: articlesAdapter }, content: true }
-  );
+  // @ts-ignore
+  const data = await getArticle(path);
+  const articleResponse = rawFullArticleAdapter(data);
 
   return articleResponse === false
     ? notFound()

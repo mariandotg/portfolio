f5e6a1d914f33cbcdd8633a5ed3ba1a55aa8e5a8
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-12 01:08:06 -0300

    Added rawFullArticleAdapter

---
+++ adapters/rawFullArticleAdapter.ts
@@ -0,0 +1,51 @@
+import { RawFullArticle } from '@/models/blog/blog.models';
+
+export const rawFullArticleAdapter = (article: RawFullArticle) => {
+  const {
+    id,
+    attributes: {
+      title,
+      content,
+      description,
+      path,
+      category,
+      canonicalUrl,
+      openGraphType,
+      schemaType,
+      publishedAt,
+      locale,
+      image,
+      localizations,
+    },
+  } = article;
+  const seo = {
+    title,
+    id,
+    description,
+    image: image.data.attributes.url,
+    imageAlt: image.data.attributes.alternativeText,
+    slug: path,
+    path,
+    openGraphType,
+    url: canonicalUrl,
+    schemaType,
+  };
+  const locales = localizations.data.map((localization, index) => {
+    const id = localization.id;
+    const locale = localization.attributes.locale;
+    return { locale, id };
+  });
+
+  return {
+    title,
+    content,
+    description,
+    path,
+    category,
+    seo,
+    publishedAt,
+    locale,
+    image,
+    locales,
+  };
+};

96f36dab613550d618191a2df7d06d8e9a61159c
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-12 01:16:26 -0300

    Refactored latest articles api route

--- app/[lang]/api/articles/latest/route.ts
+++ app/[lang]/api/articles/latest/route.ts
@@ -1,48 +1,12 @@
-import { articlesAdapter } from '@/adapters/articlesAdapter';
-import { CompoundFilterObj } from '@/models/notion/Filters';
-import { queryDatabase } from '@/services/notion';
+import { rawPreviewArticlesAdapter } from '@/adapters/rawPreviewArticlesAdapter';
+import { getLatestArticles } from '@/services/blog';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const lang = request.nextUrl.pathname.split('/').filter(Boolean)[0];
-  const databaseId = process.env.NEXT_PUBLIC_NOTION_PAGES_DATABASE_ID!;
 
-  const latestArticlesFilter: CompoundFilterObj = {
-    and: [
-      {
-        property: 'Stage',
-        select: {
-          equals: 'Published',
-        },
-      },
-      {
-        property: 'LatestArticle',
-        checkbox: {
-          equals: true,
-        },
-      },
-      {
-        property: 'Database',
-        select: {
-          equals: 'Articles Database',
-        },
-      },
-      {
-        property: 'Locale',
-        select: {
-          equals: lang,
-        },
-      },
-    ],
-  };
-
-  const articlesResponse = await queryDatabase({
-    databaseId,
-    filter: latestArticlesFilter,
-    pageSize: 3,
-  });
-
-  const articles = articlesAdapter(articlesResponse);
+  const data = await getLatestArticles(lang);
+  const articles = rawPreviewArticlesAdapter(data);
 
   return NextResponse.json([...articles]);
 }

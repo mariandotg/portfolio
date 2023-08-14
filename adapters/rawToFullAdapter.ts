110abe186d9372e3b8936b736eee676cbc59d8c9
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-14 19:36:10 -0300

    Added rawToFullProject adapter

--- adapters/rawToFullAdapter.ts
+++ adapters/rawToFullAdapter.ts
@@ -1,6 +1,12 @@
-import { RawFullArticle } from '@/models/blog/blog.models';
+import {
+  FullArticle,
+  FullProject,
+  RawFullArticle,
+  RawFullProject,
+} from '@/models/blog/blog.models';
+import { randomUUID } from 'crypto';
 
-export const rawToFull = (article: RawFullArticle) => {
+export const rawToFullArticle = (article: RawFullArticle): FullArticle => {
   const {
     id,
     attributes: {
@@ -20,7 +26,7 @@ export const rawToFull = (article: RawFullArticle) => {
   } = article;
   const seo = {
     title,
-    id,
+    id: id.toString(),
     description,
     image: image.data.attributes.url,
     imageAlt: image.data.attributes.alternativeText,
@@ -42,6 +48,64 @@ export const rawToFull = (article: RawFullArticle) => {
     description,
     path,
     category,
+    seo,
+    publishedAt,
+    locale,
+    image,
+    locales,
+  };
+};
+
+export const rawToFullProject = (project: RawFullProject): FullProject => {
+  const {
+    id,
+    attributes: {
+      title,
+      content,
+      description,
+      path,
+      tags: rawTags,
+      repository,
+      live,
+      canonicalUrl,
+      openGraphType,
+      schemaType,
+      publishedAt,
+      locale,
+      image,
+      localizations,
+    },
+  } = project;
+  const tags: { id: string; name: string }[] = rawTags.reduce((prev, curr) => {
+    const id = randomUUID();
+    return [...prev, { id, name: curr }];
+  }, [] as { id: string; name: string }[]);
+  const seo = {
+    title,
+    id: id.toString(),
+    description,
+    image: image.data.attributes.url,
+    imageAlt: image.data.attributes.alternativeText,
+    slug: path,
+    path,
+    openGraphType,
+    url: canonicalUrl,
+    schemaType,
+  };
+  const locales = localizations.data.map((localization, index) => {
+    const id = localization.id;
+    const locale = localization.attributes.locale;
+    return { locale, id };
+  });
+
+  return {
+    title,
+    content,
+    description,
+    path,
+    tags,
+    repository,
+    live,
     seo,
     publishedAt,
     locale,

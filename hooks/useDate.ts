5904238982f345f6b343a2d8aeae936f6d30d46b
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-13 22:37:31 -0300

    Fixed wrong month calculation

--- hooks/useDate.ts
+++ hooks/useDate.ts
@@ -4,7 +4,7 @@ interface FormattedDate {
 
 const useDate = (dateObject: Date) => {
   const day = dateObject.getDate().toString().padStart(2, '0');
-  const month = dateObject.getMonth().toString().padStart(2, '0');
+  const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
   const year = dateObject.getFullYear();
 
   const formattedDate: FormattedDate = {

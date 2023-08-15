eb8430fabb81889b119edf997145276c0119bfe3
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-14 23:51:39 -0300

    Refactored projects api route

--- app/[lang]/api/projects/route.ts
+++ app/[lang]/api/projects/route.ts
@@ -1,73 +1,12 @@
-import { projectsAdapter } from '@/adapters/projectsAdapter';
-import { CompoundFilterObj, FilterObj } from '@/models/notion/Filters';
-import { queryDatabase } from '@/services/notion';
+import { rawsToPreviewProjects } from '@/adapters/rawToPreviewAdapters';
+import { fetchProjects } from '@/services/blog';
 import { NextRequest, NextResponse } from 'next/server';
-
-const createFiltersFromParam = (
-  searchParams: URLSearchParams,
-  param: string
-) => {
-  const tags = searchParams.get(param);
-
-  const values = tags?.split(',');
-  const filters = values?.reduce<Array<FilterObj<{ [key: string]: unknown }>>>(
-    (acc, currValue) => {
-      return [
-        ...acc,
-        {
-          property: 'Tags',
-          multi_select: {
-            contains: currValue,
-          },
-        },
-      ];
-    },
-    []
-  );
-
-  return filters || [];
-};
 
 export async function GET(request: NextRequest) {
   const lang = request.nextUrl.pathname.split('/').filter(Boolean)[0];
-  const databaseId = process.env.NEXT_PUBLIC_NOTION_PAGES_DATABASE_ID!;
 
-  const tagsFilters = createFiltersFromParam(
-    request.nextUrl.searchParams,
-    'tags'
-  );
-
-  const projectsFilter: CompoundFilterObj = {
-    and: [
-      {
-        property: 'Stage',
-        select: {
-          equals: 'Published',
-        },
-      },
-      {
-        property: 'Database',
-        select: {
-          equals: 'Projects Database',
-        },
-      },
-      {
-        property: 'Locale',
-        select: {
-          equals: lang,
-        },
-      },
-      ...tagsFilters,
-    ],
-  };
-
-  const projectsResponse = await queryDatabase({
-    databaseId,
-    filter: projectsFilter,
-    pageSize: 10,
-  });
-
-  const projects = projectsAdapter(projectsResponse);
+  const data = await fetchProjects(lang);
+  const projects = rawsToPreviewProjects(data);
 
   return NextResponse.json({ projects });
 }

183171ce5d00fc496a3de276e656eca41eb8bdea
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-08-14 23:49:10 -0300

    Added rawToPreviewProject adapters

--- adapters/rawToPreviewAdapters.ts
+++ adapters/rawToPreviewAdapters.ts
@@ -1,4 +1,10 @@
-import { RawPreviewArticle, PreviewArticle } from '@/models/blog/blog.models';
+import {
+  RawPreviewArticle,
+  PreviewArticle,
+  RawPreviewProject,
+  PreviewProject,
+} from '@/models/blog/blog.models';
+import { randomUUID } from 'crypto';
 
 export const rawToPreviewArticle = (
   article: RawPreviewArticle
@@ -18,8 +24,36 @@ export const rawToPreviewArticle = (
   return { path, title, publishedAt, category, id, image };
 };
 
+export const rawToPreviewProject = (
+  project: RawPreviewProject
+): PreviewProject => {
+  const {
+    id,
+    attributes: { tags: rawTags, publishedAt, title, path, image: imageData },
+  } = project;
+  const tags: { id: string; name: string }[] = rawTags.reduce((prev, curr) => {
+    const id = randomUUID();
+    return [...prev, { id, name: curr }];
+  }, [] as { id: string; name: string }[]);
+  const image = {
+    placeholder: imageData.data.attributes.placeholder,
+    name: imageData.data.attributes.name,
+    alternativeText: imageData.data.attributes.alternativeText,
+    width: imageData.data.attributes.width,
+    height: imageData.data.attributes.height,
+    url: imageData.data.attributes.url,
+  };
+  return { path, title, publishedAt, tags, id, image };
+};
+
 export const rawsToPreviewArticles = (
   articles: RawPreviewArticle[]
 ): PreviewArticle[] => {
   return articles.map((article) => rawToPreviewArticle(article));
+};
+
+export const rawsToPreviewProjects = (
+  projects: RawPreviewProject[]
+): PreviewProject[] => {
+  return projects.map((project) => rawToPreviewProject(project));
 };

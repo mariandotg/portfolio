b5e5b348efdc5527dec515e4ba1827b3abfe37ca
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-09-12 17:20:15 -0300

    Added social-media content service

---
+++ services/content/social-media.ts
@@ -0,0 +1,24 @@
+import { GITHUB_TOKEN } from '@/config';
+import { SocialMedia } from '@/models/domain/SocialMedia';
+
+export const fetchSocialMedia = async (): Promise<
+  SocialMedia[] | undefined
+> => {
+  const response = await fetch(
+    `https://raw.githubusercontent.com/mariandotg/portfolio-content/main/social-media.json`,
+    {
+      headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+      cache: 'force-cache',
+    }
+  );
+
+  if (!response.ok) return undefined;
+
+  const rawMDX: SocialMedia[] = await response.json();
+
+  return rawMDX;
+};

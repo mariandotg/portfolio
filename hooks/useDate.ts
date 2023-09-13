0058d97c50ba8131d5bd040c427ef612ae8a7c2d
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-09-13 14:26:20 -0300

    Refactored useDate hook

--- hooks/useDate.ts
+++ hooks/useDate.ts
@@ -1,18 +1,72 @@
 interface FormattedDate {
-  [key: string]: string;
+  day: number;
+  paddedDay: string;
+  month: number;
+  paddedMonth: string;
+  year: number;
+  short: {
+    [index: string]: string;
+  };
+  long: {
+    [index: string]: string;
+  };
 }
 
 const useDate = (dateObject: Date) => {
-  const day = dateObject.getDate().toString().padStart(2, '0');
-  const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
+  const day = dateObject.getDate();
+  const paddedDay = day.toString().padStart(2, '0');
+  const month = dateObject.getMonth() + 1;
+  const paddedMonth = month.toString().padStart(2, '0');
   const year = dateObject.getFullYear();
 
-  const formattedDate: FormattedDate = {
-    es: `${day}/${month}/${year}`,
-    en: `${month}/${day}/${year}`,
+  const monthsArrays = {
+    es: [
+      'Enero',
+      'Febrero',
+      'Marzo',
+      'Abril',
+      'Mayo',
+      'Junio',
+      'Julio',
+      'Agosto',
+      'Septiembre',
+      'Octubre',
+      'Noviembre',
+      'Diciembre',
+    ],
+    en: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
   };
 
-  return { day, month, year, formattedDate };
+  const formattedDate: FormattedDate = {
+    day,
+    paddedDay,
+    month,
+    paddedMonth,
+    year,
+    short: {
+      es: `${day}/${month}/${year}`,
+      en: `${month}/${day}/${year}`,
+    },
+    long: {
+      es: `${monthsArrays.es[month - 1]} ${day}, ${year}`,
+      en: `${monthsArrays.en[month - 1]} ${day}, ${year}`,
+    },
+  };
+
+  return formattedDate;
 };
 
 export default useDate;

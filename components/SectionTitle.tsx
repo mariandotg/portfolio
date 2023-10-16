6ad4f66ee5b3788e0ba87dd445c2b41c01955696
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-10-16 07:23:01 -0300

    Removed emoji on SectionTitle

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -17,7 +17,7 @@ const SectionTitle = ({ children, emoji, additionalLink }: Props) => {
   return (
     <div className='flex items-end justify-between tablet:col-span-4 text-light-headlines dark:text-dark-headlines'>
       <h2 className='flex items-center w-full font-medium text-article font-display gap-x-4 '>
-        {emoji && <Emoji emoji={emoji} width={20} height={20} />}
+        {/* {emoji && <Emoji emoji={emoji} width={20} height={20} />} */}
         {children}
       </h2>
       {additionalLink && (

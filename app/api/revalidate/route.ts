616c4b5d1dbb993773a6f9793e597b21a8036ab0
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-11-14 17:29:27 -0300

    Changed api revalidate route from GET to POST

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get('secret');
 
   if (secret !== process.env.MY_SECRET_TOKEN) {

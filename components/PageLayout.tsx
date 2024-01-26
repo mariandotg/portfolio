78c14087d20351cee35fe024f404bae2c4d45d30
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-01-25 21:00:54 -0300

    Modified PageLayout

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -8,7 +8,7 @@ const PageLayout = ({ children, className }: Props) => {
   return (
     <div className='flex justify-center mt-[56px] overflow-hidden'>
       <div
-        className={`flex flex-col w-screen tablet:max-w-2xl px-4 gap-20 tablet:gap-28 tablet:border-x tablet:border-light-subtle-edges tablet:dark:border-dark-subtle-edges ${className}`}
+        className={`flex flex-col w-screen tablet:max-w-2xl px-4 gap-20 tablet:gap-28 ${className}`}
       >
         {children}
       </div>
